53b352397e1a3d4403532d5adec9f45ad07d3299
sorsicd7 <lotfaliansoroush@gmail.com>
2023-06-05 17:31:07 +0200

    Added Exercise Model

--- workout_tracker/models.py
+++ workout_tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='exercise_images/', blank=True, null=True)
+    video = models.FileField(upload_to='exercise_videos/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
