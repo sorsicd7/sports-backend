ed9146d1439b2f13d5228607a30df6a7ef00da1f
sorsicd7 <lotfaliansoroush@gmail.com>
2023-06-05 22:25:17 +0200

    Added url for WorkoutList

--- workout_tracker/urls.py
+++ workout_tracker/urls.py
@@ -3,6 +3,7 @@ from workout_tracker.views import (
     ExerciseList,
     ExerciseCreate,
     ExerciseDetail,
+    WorkoutList,
 
 )
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path('exercises/', ExerciseList.as_view(), name='exercise-list'),
     path('exercises/create/', ExerciseCreate.as_view(), name='exercise-create'),
     path('exercises/<int:pk>/', ExerciseDetail.as_view(), name='exercise-detail'),
-]
+    path('workouts/', WorkoutList.as_view(), name='workout-list'),
+ ]
