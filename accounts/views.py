b4e5219d5cd14a701d34477f5c6ae78ef216ab96
sorsicd7 <lotfaliansoroush@gmail.com>
2023-06-09 16:19:51 +0200

    Updated permissions

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,6 @@ from .serializers import AccountSerializer
 class AccountListCreateView(generics.ListCreateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save()
@@ -15,7 +14,6 @@ class AccountDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
     lookup_field = 'id'
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_update(self, serializer):
         serializer.save()
--- sports/settings.py
+++ sports/settings.py
@@ -66,7 +66,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',  
     ],
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAdminUser"],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
     'DEFAULT_THROTTLE_RATES': {
         "authentication": "8/hour",
         "verify_authentication": "8/hour",
